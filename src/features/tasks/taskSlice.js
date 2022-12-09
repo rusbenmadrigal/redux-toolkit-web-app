eb5f5e41edfc2f2dff80777864472aeebff52289
Russ Madrigal <71038002+rusbenmadrigal@users.noreply.github.com>
2022-12-08 21:37:18 -0600

    Updated CSS and Redux

--- src/features/tasks/taskSlice.js
+++ src/features/tasks/taskSlice.js
@@ -22,8 +22,24 @@ export const taskSlice = createSlice({
     addTask: (state, action) => {
       state.push(action.payload);
     },
+    editTask: (state, action) => {
+      const { id, title, description } = action.payload;
+      const taskFound = state.find((task) => task.id === id);
+      if (taskFound) {
+        taskFound.title = title;
+        taskFound.description = description;
+      }
+    },
+    deleteTask: (state, action) => {
+      const taskFound = state.find((task) => task.id === action.payload);
+      if (taskFound) {
+        const index = state.indexOf(taskFound);
+        state.splice(index, 1);
+      }
+    },
+   
   },
 });
 
-export const { addTask } = taskSlice.actions;
+export const { addTask, deleteTask, editTask } = taskSlice.actions;
 export default taskSlice.reducer;
